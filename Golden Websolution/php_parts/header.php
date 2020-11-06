cbe604556345c4567ee695160d246ec864abbbe2
Nico <bua-nico@hotmail.de>
2020-11-06 10:06:23 +0100

    Layout into Code

---
+++ Golden Websolution/php_parts/header.php
@@ -0,0 +1,37 @@
+<header class="page-header">
+        <div class="container">
+            <nav class="navbar navbar-expand-md navbar-dark">
+                <a class="navbar-brand" href="#">Golden</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav text-center">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#services">Dienstleistungen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#aboutus">Über&nbsp;uns</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#team">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="jumbotron header-jumbotron text-center d-flex flex-column justify-content-center align-items-center">
+                <h2 class="display-6">Wir entwickeln Webseiten</h2>
+                <h1 class="display-5"><span>Golden</span> Websolutions</h1>
+                <a class="btn btn-primary" href="#services" role="button">Weitere Informationen</a>
+            </div>
+        </div>
+    </header>
